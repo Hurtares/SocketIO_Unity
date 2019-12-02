087374e3ea7d52eed195d0a42686beb233020e6e
Bruno Filipe Sousa da Silva <brunosilva_95@hotmail.com>
2019-12-02 22:43:27 +0000

    First Commit

---
+++ Assets/Scripts/Networking/NetworkTransform.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(NetworkIdentity))]
+public class NetworkTransform : MonoBehaviour {
+
+    [SerializeField]
+    private Vector2 oldPosition;
+
+    private NetworkIdentity networkIdentity;
+    private Player player;
+
+    private float stillCounter = 0;
+    // Start is called before the first frame update
+    void Start() {
+        networkIdentity = GetComponent<NetworkIdentity>();
+        oldPosition = transform.position;
+        player = new Player();
+        player.position = new Position();
+        player.position.x = transform.position.x;
+        player.position.y = transform.position.y;
+        Debug.Log("Transform"+transform.position);
+
+        if (!networkIdentity.IsControlling()) {
+            enabled = false;
+        }
+    }
+
+    // Update is called once per frame
+    void Update() {
+        if (networkIdentity.IsControlling()) {
+            if (oldPosition != (Vector2)transform.position) {
+                oldPosition = transform.position;
+                stillCounter = 0;
+                sendData();
+            } else {
+                stillCounter += Time.deltaTime;
+                if (stillCounter >= 1) {
+                    stillCounter = 0;
+                    sendData();
+                }
+            }
+        }
+    }
+    void sendData() {
+        player.position.x = Mathf.Round(transform.position.x * 1000.0f) / 1000.0f;
+        player.position.y = Mathf.Round(transform.position.y * 1000.0f) / 1000.0f;
+
+        string str = "{\"id\":\"\",\"position\":{\"x\":\""+player.position.x+"\",\"y\":\""+player.position.y+"\"}}";
+
+        JSONObject js = new JSONObject(str);
+
+        networkIdentity.GetSocket().Emit("updatePosition", js);
+    }
+}
---
+++ Assets/SelfDestruct.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SelfDestruct : MonoBehaviour
+{
+    private float timer = .5f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (timer <= 0) {
+            Destroy(this.gameObject);
+        }
+        timer -= Time.deltaTime;
+    }
+    private void OnTriggerEnter(Collider other) {
+        Debug.Log("Boooooom");
+
+    }
+}
